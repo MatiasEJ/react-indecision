d8995278522a6a306311c20ca2a2fa1fbb0e2b76
keta <don.juncos@gmail.com>
2020-02-12 13:40:25 -0300

    herukeando

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require ('express');
 const path = require ('path');
 const port = process.env.PORT || 8080;
 const app = express();
-const publicPath = path.join (__dirname,'..','public');
+const publicPath = path.join (__dirname,'public');
 
 
-app.use(express.static(__dirname));
+app.use(express.static(publicPath));
 
 app.get('*', (req,res)=>{
     res.sendFile(path.resolve(publicPath, 'index.html'))
